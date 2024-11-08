6841b92d59650fdf5bf6ce0d6d1ff787511fb40a
Violet <zk1875544303@sina.com>
2024-11-08 18:43:40 +0800

    some remarks

--- 4.Reverse String/main.py
+++ 4.Reverse String/main.py
@@ -13,8 +13,9 @@ user_input = input("Enter a string: ")
 reversed_string = string_reverse(user_input)
 print("Reversed String: ",reversed_string)
 
-# 格式b = a[i:j:s],这里的s表示步长(step)，默认为1.（-1时即翻转读取）,所以a[i:j:1]相当于a[i:j]
+# 格式b = a[i:j:s],这里的s表示步长(step)，默认为1（-1时即翻转读取）;j默认为(len + 1);a[i:j:1]相当于a[i:j]
 # i取负值时，表示倒数第i个
+# a[i:j:1] 会忽略第j个数
 a = [1,2,3,4,5]
 print(a[::-1]) #倒数遍历
 print(a[2::-1])
@@ -22,3 +23,4 @@ print(a[2::1])
 print(a[2::-2]) 
 print(a[-2::-1])
 print(a[2:4:1])
+print(a[1:4:2])
